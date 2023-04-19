ad84d359cfb23baaf0086e78d2efaa2ea918efc8
Farimah71 <farimah.fattahi2000@gmail.com>
2023-04-20 00:55:00 +0430

    Add search bar

---
+++ src/components/Search-bar/Search.tsx
@@ -0,0 +1,29 @@
+import { Container, InputAdornment, TextField } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import "../Search-bar/search.scss";
+
+const Search = () => {
+  return (
+    <Container maxWidth="md">
+      <TextField
+      color="warning"
+        id="search"
+        type="search"
+        label="Search For Task..."
+        // value={searchTerm}
+        // onChange={handleChange}
+        sx={{ width: 200 }}
+        size="small"
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+    </Container>
+  );
+};
+
+export default Search;
--- src/layouts/main/Main.tsx
+++ src/layouts/main/Main.tsx
@@ -1,9 +1,11 @@
+import Search from "../../components/Search-bar/Search";
 import MyButton from "../../components/buttons/MyButton";
 import "./main.scss";
 
 const Main = () => {
   return (
     <div className="main">
+      <Search />
       <span className="remained-tasks"> 3 items left.</span>
       <MyButton text="Add new task" />
     </div>
