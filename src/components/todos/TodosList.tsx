30540b41315f5617c90e152d91c6600e821148b2
Farimah71 <farimah.fattahi2000@gmail.com>
2023-04-20 16:26:55 +0430

    Add style to todoList

--- src/components/todos/TodosList.tsx
+++ src/components/todos/TodosList.tsx
@@ -1,17 +1,40 @@
 import { useAppSelector } from "../../app/hooks";
+import EditNoteIcon from "@mui/icons-material/EditNote";
+import DeleteIcon from "@mui/icons-material/DeleteTwoTone";
+import "./todosList.scss";
 
 const TodosList = (): JSX.Element => {
   const allTasks = useAppSelector((state) => state.todos);
 
   return (
     <>
-      <ul>
-        {allTasks.map((task) => (
-          <li key={task.id}>
-            {task.task_name} | {task.date.toDateString()}
-          </li>
-        ))}
-      </ul>
+      <div className="task-list">
+        <ul className="list-group list-group-light">
+          {allTasks.map((task) => (
+            <div className="task-row">
+              <li className="list-group-item">
+                <input
+                  className="form-check-input clickable me-1"
+                  type="checkbox"
+                  value=""
+                  aria-label="..."
+                />
+                {task.task_name} | {task.date.toDateString()}
+                <span className="task-control">
+                  <EditNoteIcon
+                    color="warning"
+                    className="icon-control clickable"
+                  />
+                  <DeleteIcon
+                    color="error"
+                    className="icon-control clickable"
+                  />
+                </span>
+              </li>
+            </div>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
