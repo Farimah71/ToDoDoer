b18096c8db27b3c4d7af53adf0e8ea130b0d271a
Farimah71 <farimah.fattahi2000@gmail.com>
2023-04-20 23:52:46 +0430

    Add remainedItems count

--- src/layouts/main/Main.tsx
+++ src/layouts/main/Main.tsx
@@ -1,14 +1,27 @@
+import { useEffect, useState } from "react";
+import { useAppSelector } from "../../app/hooks";
 import Search from "../../components/Search-bar/Search";
 import MyButton from "../../components/buttons/MyButton";
 import TodosList from "../../components/todos/TodosList";
 import "./main.scss";
 
 const Main = () => {
+  const [count, setCount] = useState(0);
+  const allTasks = useAppSelector((state) => state.todos);
+
+  const al = allTasks.length;
+
+  useEffect(() => setCount(al));
+
   return (
     <div className="main">
       <div className="first-row">
         <Search />
-        <span className="remained-tasks"> 3 items left.</span>
+        <span className="remained-tasks">
+          {count === 0 && "Hey, You are free!"}
+          {count === 1 && `${count} item left.`}
+          {count > 1 && `${count} items left.`}
+        </span>
         <MyButton text="Add new task" />
       </div>
 
